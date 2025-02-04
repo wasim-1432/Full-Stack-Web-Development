113697b0cc632ddcd6a851c4210aac7104c5702d
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-04 08:14:24 +0530

    Out of all the keywords,name those keywords which are used as data.

---
+++ wasim54.py
@@ -0,0 +1,7 @@
+False - A boolean value representing "false"
+None - Represents the absence of a value or a null Value
+True - A boolean value representing "true"
+is - Used for identity comparision,indirectly relating to comparison of data 
+and - Logical operator used in boolean data logic
+or - Another logical operator used in boolean data logic
+not - Logical negation for boolean data
