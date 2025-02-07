68d0a0c92118c4a5f5bd74615305c3a3566f29e9
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-07 11:07:55 +0530

    Write a python script to calculate volume of a cuboid.

---
+++ wasim94.py
@@ -0,0 +1,4 @@
+l=int(input("Enter the length of the cuboid\n"))
+b=int(input("Enter the breath of the cuboid\n"))
+h=int(input("Enter the height of the cuboid\n"))
+print("Volume of cuboid is=",l*b*h)
