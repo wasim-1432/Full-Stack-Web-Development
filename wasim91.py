afa46ea6850544d33578edad98b2fb744990c7cd
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-07 10:45:16 +0530

    Write a python script to calculate simple interest.

---
+++ wasim91.py
@@ -0,0 +1,4 @@
+p=int(input("Enter the principle amount\n"))
+r=int(input("Enter the rate of the interest\n"))
+t=int(input("Enter the time(in Years)\n"))
+print("Simple Interest is=",(p*r*t)/100.0)
