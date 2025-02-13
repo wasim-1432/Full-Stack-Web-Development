1e827f4b698da7bc52fb82d391a2753373018b20
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-13 08:10:29 +0530

    Write a python script to take a string from a the user.If the string is a part of 'Galgotias' then print 'One' if the string is a part of 'College' the print two,and if the string is a part of 'Engineering' then print 'Three'.(Using Match Keyword).

---
+++ wasim145.py
@@ -0,0 +1,8 @@
+x=input("Enter any string\n")
+match x:
+    case x if x=="Galgotias":
+        print("One")
+    case x if x=="College":
+        print("Two")
+    case x if x=="Engineering":
+        print("Three")
