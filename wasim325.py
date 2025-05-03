494045dec23fe13809f82596630fc380edc5be72
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-05-03 10:09:51 +0530

    Write a python function to calculate volume of cuboid.

---
+++ wasim325.py
@@ -0,0 +1,5 @@
+def Volume_Of_Cuboid(length,width,height):
+    return length*width*height
+print("Enter the length,width and height")
+length,width,height=int(input()),int(input()),int(input())
+print("Volume of cuboid is=",Volume_Of_Cuboid(length,width,height))
