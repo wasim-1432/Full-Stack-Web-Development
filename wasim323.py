8010188172aa0f43a4ac93e70336b83d5e213d64
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-05-03 09:47:44 +0530

    Write a python function to calculate average of givrn three numbers.

---
+++ wasim323.py
@@ -0,0 +1,5 @@
+def Average_Of_Three_Numbers(a,b,c):
+    return (a+b+c)/3
+print("Enter any three numbers")
+a,b,c=int(input()),int(input()),int(input())
+print("Average is=",Average_Of_Three_Numbers(a,b,c))
